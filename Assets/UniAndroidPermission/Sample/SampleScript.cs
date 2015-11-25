5c52297ec6b2dde235a997c8af5c4aae087318ac
sanuki.wataru <ericsson.goodjob@gmail.com>
2015-11-25 20:12:57 +0900

    First Commit

---
+++ Assets/UniAndroidPermission/Sample/SampleScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SampleScript : MonoBehaviour {
+
+    [SerializeField] Text text;
+
+    public void RequestPermission(){
+        if (UniAndroidPermission.IsPermitted (AndroidPermission.WRITE_EXTERNAL_STORAGE)) {
+            text.text = "WRITE_EXTERNAL_STORAGE is already permitted!!";
+            return;
+        }
+
+        UniAndroidPermission.RequestPremission (AndroidPermission.WRITE_EXTERNAL_STORAGE, () => {
+            text.text = "WRITE_EXTERNAL_STORAGE is permitted NOW!!";
+        }, () => {
+            text.text = "WRITE_EXTERNAL_STORAGE id NOT permitted...";
+        });
+    }
+}
---
+++ Assets/UniAndroidPermission/UniAndroidPermission.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+#if UNITY_ANDROID
+public class UniAndroidPermission : MonoBehaviour {
+
+    private static Action permitCallBack;
+    private static Action notPermitCallBack;
+
+    void Awake(){
+        DontDestroyOnLoad (gameObject);
+    }
+
+    public static bool IsPermitted(AndroidPermission permission){
+#if UNITY_EDITOR
+        Debug.LogWarning("UniAndroidPermission works only Androud Devices.");
+        return true;
+#endif
+        AndroidJavaClass permissionManager = new AndroidJavaClass ("jp.ne.donuts.permissiongetter.PermissionGetter");
+        return permissionManager.CallStatic<bool> ("hasPermission", GetCurrentActivity(), GetPermittionStr(permission));
+    }
+
+    public static void RequestPremission(AndroidPermission permission, Action onPermit = null, Action notPermit = null){
+#if UNITY_EDITOR
+        Debug.LogWarning("UniAndroidPermission works only Androud Devices.");
+        return;
+#endif
+        AndroidJavaClass permissionManager = new AndroidJavaClass ("jp.ne.donuts.permissiongetter.PermissionGetter");
+        permissionManager.CallStatic("requestPermission", GetCurrentActivity(), GetPermittionStr(permission));
+        permitCallBack = onPermit;
+        notPermitCallBack = notPermit;
+    }
+
+    private static AndroidJavaObject GetCurrentActivity(){
+        return new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+    }
+
+    private static string GetPermittionStr(AndroidPermission permittion){
+        return "android.permission." + permittion.ToString ();
+    }
+
+    private void OnPermit(){
+        if (permitCallBack != null) {
+            permitCallBack ();
+        }
+        ResetCallBacks ();
+    }
+
+    private void NotPermit(){
+        if (notPermitCallBack != null) {
+            notPermitCallBack ();
+        }
+        ResetCallBacks ();
+    }
+
+    private void ResetCallBacks(){
+        notPermitCallBack = null;
+        permitCallBack = null;
+    }
+}
+
+// Protection level: dangerous permissions 2015/11/25
+// http://developer.android.com/intl/ja/reference/android/Manifest.permission.html
+public enum AndroidPermission{
+    ACCESS_COARSE_LOCATION,
+    ACCESS_FINE_LOCATION,
+    ADD_VOICEMAIL,
+    BODY_SENSORS,
+    CALL_PHONE,
+    CAMERA,
+    GET_ACCOUNTS,
+    PROCESS_OUTGOING_CALLS,
+    READ_CALENDAR,
+    READ_CALL_LOG,
+    READ_CONTACTS,
+    READ_EXTERNAL_STORAGE,
+    READ_PHONE_STATE,
+    READ_SMS,
+    RECEIVE_MMS,
+    RECEIVE_SMS,
+    RECEIVE_WAP_PUSH,
+    RECORD_AUDIO,
+    SEND_SMS,
+    USE_SIP,
+    WRITE_CALENDAR,
+    WRITE_CALL_LOG,
+    WRITE_CONTACTS,
+    WRITE_EXTERNAL_STORAGE
+}
+#endif
